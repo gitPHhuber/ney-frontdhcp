9493210eef7e461dc0efc2f336289e29cd703e31
gitPHhuber <gptusers.user1@gmail.com>
2025-09-21 18:00:21 +0300

    feat: add product passport constructor

--- src/pages/product-passport/ProductPassportPage.tsx
+++ src/pages/product-passport/ProductPassportPage.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { ProductPassportWizard } from '../../features/product-passport/ProductPassportWizard';
+import { ProductPassportConstructor } from '../../features/product-passport/ProductPassportConstructor';
 
-const ProductPassportPage: React.FC = () => <ProductPassportWizard />;
+const ProductPassportPage: React.FC = () => (
+  <div className="stacked-page">
+    <ProductPassportWizard />
+    <ProductPassportConstructor />
+  </div>
+);
 
 export default ProductPassportPage;
